ac90b87713178241ce28459c35bf180d0f1f98ec
Megan Buck <thequiltingriverotter@gmail.com>
2023-10-04 06:59:29 -0400

    Added solution

--- fibonacci.js
+++ fibonacci.js
@@ -1,5 +1,28 @@
 function fibonacci(num) {
-  return 0;
+  const fib_seq = [0,1];
+  if (num === 0){
+    return 0;
+  } else if (num === 1){
+    return 1;
+  } else {
+    while (fib_seq.length <= num){
+      fib_seq.push(fib_seq[fib_seq.length-1] + fib_seq[fib_seq.length-2])
+    }
+  }
+  return fib_seq[fib_seq.length-1]
 }
 
+
+// Recursive version
+// function fibonacci(num){
+//   if (num === 0){
+//     return 0;
+//   } else if (num === 1) {
+//     return 1
+//   } else {
+//     return fibonacci(num-1) + fibonacci(num-2);
+//   }
+// }
+
+console.log(fibonacci(12));
 module.exports = fibonacci;
